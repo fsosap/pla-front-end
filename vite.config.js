5a87bba527e69e742b8dfd0f2fce8a6917424cf2
Felipe Sosa <fsosap@eafit.edu.co>
2022-03-26 02:35:11 -0500

    update port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  host: '0.0.0.0',
+  server:{
+    port: 8080
+  },
   plugins: [vue()],
   resolve: {
     alias: {
